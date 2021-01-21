b8744f3c6d3e880731c60ecf48689b564cec3051
Rakshit <rakshitsahai@gmail.com>
2021-01-21 12:39:44 +0530

    rendered sign in & sign up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,15 @@ module.exports.profile = function(req, res){
         title: 'username'
     });
 }
+
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: 'Codeial | Sign Up'
+    });
+}
+
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: 'Codeial | Sign In'
+    });
+}
